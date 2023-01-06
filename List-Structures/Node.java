6f466d7f9773eda1c66458617cb37f0a799bbb91
Brayden Shkwarok <brady82@live.ca>
2023-01-06 14:08:25 -0800

    add node file to folder

---
+++ List-Structures/Node.java
@@ -0,0 +1,42 @@
+public class Node<T> {
+    /*
+     * Node class defines the basic element to be useed in a linked list
+     * style of data structure.
+     * 
+     * This node class is generic and can be used with any object type
+     */
+
+    T data;
+    Node<T> next;
+    Node<T> prev;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public void setData(T newData) {
+        this.data = newData;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+}
