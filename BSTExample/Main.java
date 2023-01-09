d7e9210a069f74a726a9d83015f74c7797dd4a54
Brayden Shkwarok <brady82@live.ca>
2023-01-09 13:27:15 -0800

    running/testing BSTExample

--- BSTExample/Main.java
+++ BSTExample/Main.java
@@ -1,5 +1,6 @@
 package BSTExample;
 import TreeStructures.*; // * is used to insert all the methods from the TreeStructures package
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,9 @@ public class Main {
 
         arrToTreeInsert(bst,keys,values);
 
+        printTree(bst,1);
+        printTree(bst,2);
+        printTree(bst,3);
 
     }
 
@@ -18,5 +22,33 @@ public class Main {
         for (int i=0; i<keys.length; i++) {
             bst.insert(keys[i],values[i]);
         }
+    }
+
+    public static void printTree(BinarySearchTree<Integer,String> bst, int orderType) {
+       
+        List<Entry<Integer,String>> entryList;
+        switch (orderType) {
+            case 2:
+                System.out.println("postOrder");
+                entryList = bst.entryList(2);
+                break;
+            case 3:
+                System.out.println("inOrder");
+                entryList = bst.entryList(3);
+                break;
+            default:
+                System.out.println("preOrder");
+                entryList = bst.entryList(1);
+            break;
+        }
+
+        Iterator<Entry<Integer,String>> iter = entryList.iterator();
+
+        while (iter.hasNext()) {
+            Entry<Integer,String> item = iter.next();
+            System.out.print(item + ",");
+        }
+        System.out.println("\n");
+
     }
 }
